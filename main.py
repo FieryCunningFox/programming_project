d6b53c87ac010cc10da17487b0d2898b119d90fa
SunDancer88 <91421235+SunDancer88@users.noreply.github.com>
2021-10-05 18:33:49 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import pygame
-import random
-import sys
-import math
+import random, sys, math
 
 from pygame.locals import (K_UP, K_DOWN, K_LEFT, K_RIGHT, K_ESCAPE, KEYDOWN, QUIT,
                            RLEACCEL, )  # Import pygame.locals for easier access to key coordinates
@@ -11,54 +9,148 @@ from math import sqrt, pow
 
 pygame.init()  # connect all functions of libruary
 
+# size of display
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
 
-def buble(x, y, i):
-    screen.blit(ALL_BUBLE_IMG[i], (x, y))
 
-
+# MINI GAME DINO
 def jump():
-    global user_y
-    global jump_counter
-    global make_jump
-    if jump_counter >= -30:
-        user_y -= jump_counter // 2.5
+    global user_y, jump_counter, make_jump
+    if jump_counter >= -35:
+        user_y -= jump_counter / 2.5
         jump_counter -= 1
         if user_y >= 480:
             user_y = 480
     else:
-        jump_counter = 30
+        jump_counter = 35
         make_jump = False
         if user_y >= 480:
             user_y = 480
-
-
-def isCollision(user_x, user_y, cactus_x, cactus_y):
-    distance = sqrt(pow((user_x + 32 - cactus_x), 2) + pow((user_y - cactus_y), 2))
-    if distance < 150:
-        return True
-    else:
-        return False
-
-
-# size of display
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
 
 # dino in playing
 user_x = SCREEN_WIDTH // 4
 user_y = SCREEN_HEIGHT - 120
 
-cactus_x = SCREEN_WIDTH - 50  # blocks
-cactus_y = SCREEN_HEIGHT - 150
-num_of_cactus = 3
-all_cactus = []
-for i in range(num_of_cactus):
-    all_cactus.append([cactus_x + i * randint(200, 350), cactus_y - randint(0, 50)])
+class Cactus:
+    def __init__(self, x, y, width, image, speed):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.image = image
+        self.speed = speed
 
-floor_x = 0  # position of ground
+    def move(self):
+        if self.x >= -50:
+            screen.blit(self.image, (self.x, self.y))
+            self.x -= self.speed
+            return True
+        else:
+            self.x = SCREEN_WIDTH + 100 + randint(-80, 100)
+            return False
 
-make_jump = False
-jump_counter = 30
+
+def creat_cactus_arr(array):
+    global cactus_image, cactus_option, SCREEN_WIDTH
+    choice = randint(0, 2)  #first
+    img = cactus_image[choice]
+    width = cactus_option[choice * 2]
+    height = cactus_option[choice * 2 + 1]
+    array.append(Cactus(SCREEN_WIDTH + 20, height, width, img, 4))
+
+    choice = randint(0, 2)  # second
+    img = cactus_image[choice]
+    width = cactus_option[choice * 2]
+    height = cactus_option[choice * 2 + 1]
+    array.append(Cactus(SCREEN_WIDTH + 300, height, width, img, 4))
+
+    choice = randint(0, 2)  # third
+    img = cactus_image[choice]
+    width = cactus_option[choice * 2]
+    height = cactus_option[choice * 2 + 1]
+    array.append(Cactus(SCREEN_WIDTH + 600, height, width, img, 4))
+
+    return array
+
+
+def find_radius(array):
+    global SCREEN_WIDTH
+    maximum = max(array[0].x, array[1].x, array[2].x)
+    radius = 0
+    if maximum < SCREEN_WIDTH:
+        radius = SCREEN_WIDTH
+        if radius - maximum < 50:
+            radius += 150
+        else:
+            radius = maximum
+
+    choice = randint(0, 5)
+    if choice == 0:
+        radius += randint(10, 15)
+    else:
+        radius += randint(200, 350)
+    return radius
+
+def draw_array(array):
+    global cactus_option, cactus_image, SCREEN_WIDTH
+    for i in range(3):
+        check = array[i].move()
+        if not check:
+            radius = find_radius(array)
+
+            choice = randint(0, 2)
+            img = cactus_image[choice]
+            width = cactus_option[choice * 2]
+            height = cactus_option[choice * 2 + 1]
+            array[i] = (Cactus(SCREEN_WIDTH + 100 + randint(-80, 60), height, width, img, 4))
+    return array
+
+
+BLOCKS = []  # list of bloks for dino
+cactus_image = []
+cactus_option = [50, 400, 80, 352, 60, 450, 40, 450]
+
+block = pygame.image.load("block.png")
+block.set_colorkey((255, 255, 255))
+cactus_image.append(block)
+block2 = pygame.image.load("block2.png")
+block2.set_colorkey((255, 255, 255))
+cactus_image.append(block2)
+block3 = pygame.image.load("block3.png")
+block3.set_colorkey((255, 255, 255))
+cactus_image.append(block3)
+block4 = pygame.image.load("block4.png")
+block4.set_colorkey((255, 255, 255))
+cactus_image.append(block4)
+
+
+BLOCKS = creat_cactus_arr(BLOCKS)
+
+floor_x = 0  # position of ground FOR MOVING
+
+make_jump = False  # DINO jump
+jump_counter = 35
+
+
+# FOR LOADING
+def buble(x, y, i):
+    screen.blit(ALL_BUBLE_IMG[i], (x, y))
+
+
+ALL_BUBLE_IMG = []
+ALL_BUBLEX = []
+ALL_BUBLEY = []
+ALL_BUBLEX_CHANGE = []
+ALL_BUBLEY_CHANGE = []
+num_of_bubles = 50
+
+# loading animation
+for i in range(num_of_bubles):
+    ALL_BUBLE_IMG.append(pygame.image.load("ghost.png"))
+    ALL_BUBLEX.append(randint(20, 780))
+    ALL_BUBLEY.append(randint(20, 550))
+    ALL_BUBLEX_CHANGE.append(5)
+    ALL_BUBLEY_CHANGE.append(40)
 
 # ALL COLORS
 WHITE = (255, 255, 255)
@@ -71,28 +163,12 @@ PURPLE = (255, 0, 255)
 CYAN = (255, 0, 255)
 ALL_COLORS = [WHITE, RED, GREEN, BLUE, YELLOW, ORANGE, PURPLE, CYAN]
 
-FPS = 70  # how often cadrs will change
+FPS = 60  # how often cadrs will change
 clock = pygame.time.Clock()
 
 # sounds in game
 menu_sound = pygame.mixer.Sound("bells.mp3")
 sound = pygame.mixer.Sound("sound.mp3")
-
-ALL_BUBLE_IMG = []
-ALL_BUBLEX = []
-ALL_BUBLEY = []
-ALL_BUBLEX_CHANGE = []
-ALL_BUBLEY_CHANGE = []
-num_of_bubles = 50
-
-# loading animation
-for i in range(num_of_bubles):
-    image = pygame.image.load("ghost.png")
-    ALL_BUBLE_IMG.append(image)
-    ALL_BUBLEX.append(randint(20, 780))
-    ALL_BUBLEY.append(randint(20, 550))
-    ALL_BUBLEX_CHANGE.append(5)
-    ALL_BUBLEY_CHANGE.append(40)
 
 # counters of scene
 timer = 0
@@ -168,7 +244,6 @@ while running:
                 ALL_BUBLEX[i] += ALL_BUBLEX_CHANGE[i]
                 ALL_BUBLEY_CHANGE[i] = randint(2, 6)
                 ALL_BUBLEY[i] += ALL_BUBLEY_CHANGE[i]
-            buble(ALL_BUBLEX[i], ALL_BUBLEY[i], i)
             if ALL_BUBLEY[i] <= 0:
                 ALL_BUBLEY_CHANGE[i] = randint(2, 6)
                 ALL_BUBLEY[i] += ALL_BUBLEY_CHANGE[i]
@@ -179,6 +254,7 @@ while running:
                 ALL_BUBLEY[i] += ALL_BUBLEY_CHANGE[i]
                 ALL_BUBLEX_CHANGE[i] = -randint(2, 6)
                 ALL_BUBLEX[i] += ALL_BUBLEX_CHANGE[i]
+            buble(ALL_BUBLEX[i], ALL_BUBLEY[i], i)
 
         if timer > 500:
             counter += 1
@@ -210,19 +286,7 @@ while running:
         user.set_colorkey((255, 255, 255))
         screen.blit(user, user_rect)
 
-
-        block = pygame.image.load("block.png").convert_alpha()
-        block.set_colorkey((255, 255, 255))
-        for i in range(num_of_cactus):
-            collision = isCollision(user_x, user_y, all_cactus[i][0], all_cactus[i][1])
-            if collision:
-                status = "prepare"
-            if all_cactus[i][0] >= -50:
-                screen.blit(block, (all_cactus[i][0], all_cactus[i][1]))
-                all_cactus[i][0] -= 4
-            else:
-                all_cactus[i][0] = SCREEN_WIDTH + 20
-
+        draw_array(BLOCKS)
 
         floor = pygame.image.load("ground.png")
         floor_x -= 4
