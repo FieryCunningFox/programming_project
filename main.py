7357370c5c8d596a01a68059e51420cb81a0e014
SunDancer88 <91421235+SunDancer88@users.noreply.github.com>
2021-10-20 14:56:50 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -54,6 +54,243 @@ def pause():
         clock.tick(15)
 
 
+# MINI GAME MARIO
+tile_size = 30
+game_over = 0
+
+class Player():  # CREAT PERSON
+
+    def __init__(self, x, y):
+        self.images_right = []
+        self.images_left = []
+        self.index = 0
+        self.counter = 0
+        for num in range(1, 5):
+            img_right = pygame.image.load(f'guy{num}.png')
+            img_right = pygame.transform.scale(img_right, (35, 65))
+            img_left = pygame.transform.flip(img_right, True, False)
+            self.images_right.append(img_right)
+            self.images_left.append(img_left)
+
+        self.image = self.images_right[self.index]
+        self.dead_image = pygame.image.load('ghost.png')
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.vel_y = 0
+        self.jumped = False
+        self.direction = 0
+        self.width = self.image.get_width()
+        self.height = self.image.get_height()
+
+
+    def update(self, game_over):
+
+        walk_cooldown = 5
+        dx = 0
+        dy = 0
+
+        if game_over == 0:
+
+            key = pygame.key.get_pressed()
+            if key[pygame.K_j]:
+                dx -= 3
+                self.counter += 1
+                self.direction -= 1
+            if key[pygame.K_k]:
+                dx += 3
+                self.counter += 1
+                self.direction = 1
+            if key[pygame.K_SPACE] and self.jumped == False:
+                self.vel_y -= 11
+                self.jumped = True
+            if key[pygame.K_SPACE] == False:
+                self.jumped = False
+            if key[pygame.K_LEFT] == False and key[pygame.K_RIGHT] == False:
+                self.counter = 0
+                self.direction = 0
+                if self.direction == 1:
+                    self.images_right[self.index]
+                if self.direction == -1:
+                    self.images_left[self.index]
+
+            if self.counter > walk_cooldown:
+                self.counter = 0
+                self.index += 1
+                if self.index >= len(self.images_right):
+                    self.index = 0
+                if self.direction == 1:
+                    self.image = self.images_right[self.index]
+                if self.direction == -1:
+                    self.image = self.images_left[self.index]
+
+            self.vel_y += 1  # гравитация
+            if self.vel_y > 10:
+                self.vel_y = 10
+            dy += self.vel_y
+
+            for tile in world.tile_list:
+                if tile[1].colliderect(self.rect.x + dx, self.rect.y, self.width, self.height):
+                    dx = 0
+                if tile[1].colliderect(self.rect.x + dx, self.rect.y + dy, self.width, self.height):
+                    if self.vel_y < 0:
+                        dx= tile[1].bottom - self.rect.top
+                        self.vel_y = 0
+                    elif self.vel_y >= 0:
+                        dy = tile[1].top - self.rect.bottom
+                        self.vel_y = 0
+
+            if pygame.sprite.spritecollide(self, blob_group, False):
+                game_over = -1
+            if pygame.sprite.spritecollide(self, lava_group, False):
+                game_over = -1
+
+            self.rect.x += dx
+            self.rect.y += dy
+
+            if self.rect.bottom > SCREEN_HEIGHT:
+                self.rect.bottom = SCREEN_HEIGHT
+                dy = 0
+
+        elif game_over == -1:
+            self.image = self.dead_image
+            if self.rect.y > -50:
+                 self.rect.y -= 5
+
+        screen.blit(self.image, self.rect)
+        return game_over
+
+
+class World():
+
+    def __init__(self, data):
+        self.tile_list = []
+
+        dirt_image = pygame.image.load('dirt.png')
+        grass_image = pygame.image.load('grass.png')
+
+        row_count = 0
+        for row in data:
+            col_count = 0
+            for tile in row:
+                if tile == 1:
+                    img = pygame.transform.scale(dirt_image, (tile_size, tile_size))
+                    img_rect = img.get_rect()
+                    img_rect.x = col_count * tile_size
+                    img_rect.y = row_count * tile_size
+                    tile = (img, img_rect)
+                    self.tile_list.append(tile)
+                if tile == 2:
+                    img = pygame.transform.scale(grass_image, (tile_size, tile_size))
+                    img_rect = img.get_rect()
+                    img_rect.x = col_count * tile_size
+                    img_rect.y = row_count * tile_size
+                    tile = (img, img_rect)
+                    self.tile_list.append(tile)
+                if tile == 3:
+                    blob = Enemy(col_count * tile_size, row_count * tile_size + 7)
+                    blob_group.add(blob)
+                if tile == 7:
+                    platform = Platform(col_count * tile_size, row_count * tile_size + 7)
+                    platform_group.add(platform)
+                if tile == 6:
+                    lava = Lava(col_count * tile_size, row_count * tile_size + (tile_size // 2))
+                    lava_group.add(lava)
+                col_count += 1
+            row_count += 1
+
+    def draw(self):
+        for tile in self.tile_list:
+            screen.blit(tile[0], tile[1])
+            pygame.draw.rect(screen, (255, 255, 255), tile[1], 2)
+
+
+class Platform(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load('platform.png')
+        self.image = pygame.transform.scale(self.image, (30, 15))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.move_direction = 1
+        self.move_counter = 0
+
+    def update(self):
+        self.rect.y += self.move_direction
+        self.move_counter += 1
+        if abs(self.move_counter) > 30:
+            self.move_direction *= -1
+            self.move_counter *= -1
+
+    def draw(self):
+        for tile in self.tile_list:
+            screen.blit(tile[0], tile[1])
+            pygame.draw.rect(screen, (255, 255, 255), tile[1], 2)
+
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load('blob.png')
+        self.image = pygame.transform.scale(self.image, (24, 18))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.move_direction = 1
+        self.move_counter = 0
+
+    def update(self):
+        self.rect.x += self.move_direction
+        self.move_counter += 1
+        if abs(self.move_counter) > 25:
+            self.move_direction *= -1
+            self.move_counter *= -1
+
+    def draw(self):
+        for tile in self.tile_list:
+            screen.blit(tile[0], tile[1])
+            pygame.draw.rect(screen, (255, 255, 255), tile[1], 2)
+
+
+class Lava(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        img = pygame.image.load('lava.png')
+        self.image = pygame.transform.scale(img, (tile_size, tile_size // 2))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
+world_data = [
+[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8, 1],
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 2, 2, 1],
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 0, 7, 0, 5, 0, 0, 0, 1],
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+[1, 7, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+[1, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 0, 0, 7, 0, 0, 0, 0, 1],
+[1, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 0, 7, 0, 0, 3, 0, 0, 0, 3, 0, 0, 2, 2, 1],
+[1, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 0, 0, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 1],
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 1],
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 2, 0, 0, 2, 2, 2, 2, 2, 1],
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 6, 1, 6, 6, 6, 6, 6, 1, 1, 1, 1, 1, 1],
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+[1, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+[1, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+]
+
+player_MARIO = Player(50, SCREEN_HEIGHT - 65)
+blob_group = pygame.sprite.Group()
+lava_group = pygame.sprite.Group()
+platform_group = pygame.sprite.Group()
+world = World(world_data)
+
+
 # MINI GAME DINO
 def jump():
     global user_y, jump_counter, make_jump
@@ -299,7 +536,7 @@ sound = pygame.mixer.Sound("sound.mp3")
 # counters of scene
 timer = 0
 loading = 0
-Status = ["menu", "loading", "parametrs", "play", "dino", "fortune", "move_return", "prison"]
+Status = ["menu", "loading", "parametrs", "play", "dino", "fortune", "move_return", "prison", "mario", "bankrupt", "birthday"]
 counter = 0
 status = Status[counter]
 n = 3
@@ -318,7 +555,8 @@ while running:
             if event.key == K_ESCAPE:  # exit with key esc
                 running = False
             if event.key == pygame.K_RIGHT:  # OPEN NEXT SCENE
-                counter += 1
+                if counter <= 9:
+                    counter += 1
                 status = Status[counter]
             if event.key == pygame.K_LEFT:  # OPEN LAST SCENE
                 counter -= 1
@@ -330,7 +568,6 @@ while running:
                     make_jump = True
                 else:
                     make_jump = False
-
 
         if event.type == pygame.QUIT:
             running = False
@@ -390,11 +627,12 @@ while running:
                         counter += 4
                         status = "prison"
 
-                if pygame.mouse.get_pos() >= (30, 470):  # push cubics, get random game
+                if pygame.mouse.get_pos() >= (30, 470):  # push cubics, get game
                     if pygame.mouse.get_pos() <= (130, 570):
-                        if n >= 7:
-                            n = 2
-                            counter = 2
+                        flag = 0
+                        if n >= 9:
+                            n = 3
+                            counter = 3
                         else:
                             n += 1
                         status = Status[n]
@@ -429,6 +667,16 @@ while running:
                             else:
                                 player += 1
 
+                if flag == 4:  # when the message of mario
+                    if pygame.mouse.get_pos() >= (250, 250):
+                        if pygame.mouse.get_pos() <= (550, 450):
+                            flag = 0
+                            dino_score = 0
+                            if player == num_of_players - 1:
+                                player = 0
+                            else:
+                                player += 1
+
             if counter == 5:  # the wheel of fortune
                 if pygame.mouse.get_pos() >= (200, 150):
                     if pygame.mouse.get_pos() <= (400, 550):
@@ -456,6 +704,24 @@ while running:
                         counter = 3
                         status = Status[counter]
                         flag = 2
+
+            if counter == 9:  # bankrupt
+                if pygame.mouse.get_pos() >= (0, 0):
+                    if pygame.mouse.get_pos() <= (800, 600):
+                        list_scores[player] += 0
+                        counter = 3
+                        status = Status[counter]
+
+            if counter == 10:  # birthday
+                if pygame.mouse.get_pos() >= (260, 290):
+                    if pygame.mouse.get_pos() <= (520, 555):
+                        list_scores[player] += 150 * (num_of_players - 1)
+                        for i in range(num_of_players):
+                            if i != player:
+                                list_scores[i] -= 150
+                        counter = 3
+                        status = Status[counter]
+                        flag = 5
 
         if counter == 2:  # check cursor
             if pygame.mouse.get_pos() >= (390, 180):  # selected 2
@@ -598,14 +864,21 @@ while running:
         screen.blit(start_return, (130, 170))
         start_prison = pygame.image.load("forth.png")
         screen.blit(start_prison, (220, 170))
+        start_mario = pygame.image.load("fiveth.png")
+        screen.blit(start_mario, (290, 110))
+        start_bunkrupt = pygame.image.load("sixth.png")
+        screen.blit(start_bunkrupt, (420, 100))
+        start_birthday = pygame.image.load("seventh.png")
+        screen.blit(start_birthday, (400, 300))
+        start_logik = pygame.image.load("eighth.png")
+        screen.blit(start_logik, (480, 230))
+
 
         score_message = pygame.image.load("score.png")
         score_message.set_colorkey((255, 255, 255))
         screen.blit(score_message, (550, 20))
         print_message('player ' + str(player + 1), 400, 70)
         print_score(str(list_scores[player]), 600, 70)
-        print(list_scores)
-        print(n)
 
         screen.blit(kubs, (30, 470))
 
@@ -638,6 +911,15 @@ while running:
             print_message("CONGRATULATIONS!", 280, 300)
             print_message("YOU GOT ", 310, 320)
             print_message(str(dino_score), 350, 350)
+
+        if flag == 4:  # game over message from mario
+            mario_message = pygame.image.load("mario_message.png")
+            screen.blit(mario_message, (250, 250))
+            print_message("you lose 200", 300, 350)
+
+        if flag == 5:  # message from birthday
+            pass
+
 
     if status == "dino":
         background = pygame.image.load("play.png").convert()
@@ -695,6 +977,40 @@ while running:
         screen.blit(background, (0, 0))
         prison_exit = pygame.image.load("go_out.png")
         screen.blit(prison_exit, (prison_x, prison_y))
+
+    if status == "mario":
+        counter = 8
+        background = pygame.image.load("mario.png")
+        screen.blit(background, (0, 0))
+        world.draw()
+
+        if game_over == 0:
+            blob_group.update()
+            platform_group.update()
+        else:
+            if player_MARIO.rect.y < -30:
+                list_scores[player] -= 200
+                counter = 3
+                status = "play"
+                flag = 4
+
+
+        blob_group.draw(screen)
+        lava_group.draw(screen)
+        platform_group.draw(screen)
+
+        game_over = player_MARIO.update(game_over)
+
+    if status == 'bankrupt':
+        counter = 9
+        background = pygame.image.load("bankrupt.png")
+        screen.blit(background, (0, 0))
+
+    if status == "birthday":
+        counter = 10
+        background = pygame.image.load("birthday.png")
+        screen.blit(background, (0, 0))
+
 
     pygame.display.flip()  # update the window
     clock.tick(FPS)
